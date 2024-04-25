968095a93830ec657b3d5999c2143b5906b6e5f4
iqramalik34 <iqrazaheer577@gmail.com>
2024-04-24 21:07:18 -0700

    added screens, login, home, onboarding, register, verify otp, reset credentials, create request, donateblood,sent successfully, fragment history,fragment inbox, fragment notification, fragment home,

---
+++ app/src/main/java/com/welfare/blood/donation/DonateBloodActivity.kt
@@ -0,0 +1,11 @@
+package com.welfare.blood.donation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonateBloodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donate_blood)
+    }
+}
--- app/src/main/java/com/welfare/blood/donation/HomeActivity.kt
+++ app/src/main/java/com/welfare/blood/donation/HomeActivity.kt
@@ -2,10 +2,42 @@ package com.welfare.blood.donation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.welfare.blood.donation.fragments.HistoryFragment
+import com.welfare.blood.donation.fragments.HomeFragment
+import com.welfare.blood.donation.fragments.InboxFragment
+import com.welfare.blood.donation.fragments.NotificationFragment
 
 class HomeActivity : AppCompatActivity() {
+    val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val homeFragment=HomeFragment()
+        val inboxFragment=InboxFragment()
+        val notificationFragment=NotificationFragment()
+        val historyFragment=HistoryFragment()
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.inbox -> setCurrentFragment(inboxFragment)
+                R.id.notification -> setCurrentFragment(notificationFragment)
+                R.id.history -> setCurrentFragment(historyFragment)
+            }
+            true
+        }
+
     }
-}
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+    }
